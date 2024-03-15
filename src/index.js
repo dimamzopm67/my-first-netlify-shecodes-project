8fb3d5d2f238281896daf50e66bbeace9a962b92
dimamzopm67 <139990916+dimamzopm67@users.noreply.github.com>
2024-03-15 22:54:39 +0200

    Inject the HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -52,7 +52,35 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayforecast() {
+
+ let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecast Html="";
+
+  days.forEach(function (day) {
+   forecastHtml=
+    forecastHtml+
+    `
+     <div class="weather-forecast--day">
+      <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">⛅<div>
+            <div class = "weather-forecast-temperatures">
+              <div class = "weather-forecast-temperature">
+                <strong>15</strong>
+                </div>
+              </div class="weather-forecast-temperature">9"</div>
+              </div>
+              </div>
+   `;
+  });
+
+}
+
+  let forecast = document.querySelector("#forecast");
+foreElement.innerHTML = forecastHtml
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+displayforecast();
